8d95326cd3436715b73ddf41763afb68c9d9c20f
lenovo_notebook <merc.tm@gmail.com>
2022-02-22 12:11:31 +0300

    SQL запросы к dataframe

---
+++ main.py
@@ -0,0 +1,23 @@
+from pandasql import sqldf
+import pandas as pd
+import numpy as np
+
+users_id = np.arange(1, 1000)
+prod_id = np.arange(1, 100)
+
+action = ['камень', 'ножницы', 'бумага']
+act = np.random.choice(action, 999)
+
+start_date = pd.to_datetime('2022-01-01')
+times = pd.date_range(start_date, periods=999, freq='D')
+
+df = pd.DataFrame({'id': users_id,
+                   'date': times})
+df['action'] = act
+# print(df.groupby(['action']).size())
+
+sql = """Select * from df 
+      where action = 'камень'
+      """
+
+print(sqldf(sql))
